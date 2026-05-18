b8770de45381bd93c58362348bc6965ea73e4f57
Bridget Amana <amanabridget03@gmail.com>
2026-05-18 17:09:45 +0100

    fix: Correct regex patterns and improve card color

--- src/components/PinBoard.jsx
+++ src/components/PinBoard.jsx
@@ -4,10 +4,10 @@ export default function Pinboard() {
   const [type, setType] = useState([]);
 
   const urlRegex =
-    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
 
   const colorRegex =
-    /(?:#|0x)(?:[a-f0-9]{3}|[a-f0-9]{6})\b|(?:rgb|hsl)a?\([^\)]*\)/i;
+    /(?:#|0x)(?:[a-f0-9]{3}|[a-f0-9]{6})\b|(?:rgb|hsl)a?\([^)]*\)/i;
 
   function pasteData(e) {
     e.preventDefault();
@@ -23,12 +23,32 @@ export default function Pinboard() {
   }
 
   return (
-    <div
-      className="h-screen cursor-grab outline-0"
-      onPaste={pasteData}
-    >
+    <div className="h-screen cursor-grab outline-0" onPaste={pasteData}>
       {type.map((item, index) => (
-        <div key={index}> {item.type}: {item.value}</div>
+        <div
+          key={index}
+          className="p-4 rounded-xl bg-card w-2xs relative group"
+        >
+          <button className="absolute top-3 right-4 hidden group-hover:flex p-1 rounded-md hover:text-red-800 hover:bg-red-50 hover:cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width={16}
+              height={16}
+              color={"currentColor"}
+              fill={"none"}
+            >
+              <path
+                d="M18 6L6.00081 17.9992M17.9992 18L6 6.00085"
+                stroke="#141B34"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+          {item.type}: {item.value}
+        </div>
       ))}
     </div>
   );
