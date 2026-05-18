56dc5b48539abffbac41e329b92991ba87b3bb7b
Bridget Amana <amanabridget03@gmail.com>
2026-05-18 23:24:14 +0100

    feat: update Pinboard functionality with draggable pins

--- src/components/PinBoard.jsx
+++ src/components/PinBoard.jsx
@@ -1,36 +1,80 @@
 import { useState } from "react";
 
 export default function Pinboard() {
-  const [type, setType] = useState([]);
-  let pinboardArray = [];
+  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
+  const [draggingIndex, setDraggingIndex] = useState(null);
 
-  localStorage.setItem("pinboard_pins", JSON.stringify(pinboardArray));
+  const [type, setType] = useState([]);
 
   const urlRegex =
     /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
-
   const colorRegex =
     /(?:#|0x)(?:[a-f0-9]{3}|[a-f0-9]{6})\b|(?:rgb|hsl)a?\([^)]*\)/i;
 
   function pasteData(e) {
     e.preventDefault();
     const pastedText = e.clipboardData.getData("text/plain");
+    const startX = 100 + type.length * 10;
+    const startY = 100 + type.length * 10;
+
+    const basePin = {
+      value: pastedText,
+      x: startX,
+      y: startY,
+    };
 
     if (urlRegex.test(pastedText)) {
-      setType((current) => [...current, { type: "url", value: pastedText }]);
+      setType((current) => [...current, { ...basePin, type: "url" }]);
     } else if (colorRegex.test(pastedText)) {
-      setType((current) => [...current, { type: "color", value: pastedText }]);
+      setType((current) => [...current, { ...basePin, type: "color" }]);
     } else {
-      setType((current) => [...current, { type: "text", value: pastedText }]);
+      setType((current) => [...current, { ...basePin, type: "text" }]);
     }
   }
 
+  function handleMouseMove(e) {
+    if (draggingIndex !== null) {
+      const boardEdge = e.currentTarget.getBoundingClientRect();
+      const mouseXOnBoard = e.clientX - boardEdge.left;
+      const mouseYOnBoard = e.clientY - boardEdge.top;
+      setType((current) => {
+        const newPins = [...current];
+        newPins[draggingIndex] = {
+          ...newPins[draggingIndex],
+          x: mouseXOnBoard - dragOffset.x,
+          y: mouseYOnBoard - dragOffset.y,
+        };
+        return newPins;
+      });
+    }
+  }
+
+  function handleMouseDown(e, index) {
+    setDraggingIndex(index);
+    const pinEdge = e.currentTarget.getBoundingClientRect();
+    setDragOffset({
+      x: e.clientX - pinEdge.left,
+      y: e.clientY - pinEdge.top,
+    });
+  }
+
   return (
-    <div className="h-screen cursor-grab outline-0" onPaste={pasteData}>
+    <div
+      className="h-screen cursor-grab outline-0"
+      onMouseMove={handleMouseMove}
+      onMouseUp={() => setDraggingIndex(null)}
+      onMouseLeave={() => setDraggingIndex(null)}
+      onPaste={pasteData}
+    >
       {type.map((item, index) => (
         <div
           key={index}
-          className="p-4 rounded-xl bg-card w-2xs relative group"
+          className="p-4 rounded-xl bg-card w-2xs group cursor-grab absolute"
+          onMouseDown={(e) => handleMouseDown(e, index)}
+          style={{
+            left: `${item.x}px`,
+            top: `${item.y}px`,
+          }}
         >
           <button className="absolute top-3 right-4 hidden group-hover:flex p-1 rounded-md hover:text-red-800 hover:bg-red-50 hover:cursor-pointer">
             <svg
